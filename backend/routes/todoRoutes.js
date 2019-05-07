d5ed2a4e855bbc61ef3f95fb345aefaa480eb1d0
Grzegorz Oleksiuk <goleksiukk@gmail.com>
2019-05-07 19:41:04 +0200

    Small code refactor

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,19 +1,7 @@
-let Todo = require("../models/todo.model");
 let Material = require("../models/material_model");
 var Cart = require("../models/cart.model");
 const express = require("express");
 const todoRoutes = express.Router();
-
-//req - reques object' res = resonse object
-todoRoutes.route("/").get(function(req, res) {
-  Todo.find(function(err, todos) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(todos);
-    }
-  });
-});
 
 todoRoutes.route("/getMaterials").get(function(req, res) {
   Material.find({}, req.query.details, function(err, materials) {
@@ -46,49 +34,6 @@ todoRoutes.route("/getMaterials/selectCategory").get(function(req, res) {
       res.json(materials);
     }
   }).sort({ material_unitPrice: req.query.sort });
-});
-
-todoRoutes.route("/:id").get(function(req, res) {
-  let id = req.params.id;
-  Todo.findById(id, function(err, todo) {
-    // res.json(todo);
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(todo);
-    }
-  });
-});
-
-todoRoutes.route("/add").post(function(req, res) {
-  let todo = new Todo(req.body);
-  todo
-    .save()
-    .then(todo => {
-      res.status(200).json({ todo: "todo added successfully" });
-    })
-    .catch(err => {
-      res.status(400).send("adding new todo failed");
-    });
-});
-
-todoRoutes.route("/update/:id").post(function(req, res) {
-  Todo.findById(req.params.id, function(err, todo) {
-    if (!todo) res.status(404).send("data is not found");
-    else todo.todo_description = req.body.todo_description;
-    todo.todo_responsible = req.body.todo_responsible;
-    todo.todo_priority = req.body.todo_priority;
-    todo.todo_completed = req.body.todo_completed;
-
-    todo
-      .save()
-      .then(todo => {
-        res.json("Todo updated");
-      })
-      .catch(err => {
-        res.status(400).send("Update not possible");
-      });
-  });
 });
 
 todoRoutes.route("/addMaterial").post(function(req, res) {
