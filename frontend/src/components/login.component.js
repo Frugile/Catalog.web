29984964bcd27bb4977893b44a1225fe3a365db0
Karol <najlepszy.karol@gmail.com>
2019-05-07 19:42:42 +0200

    google id added

--- frontend/src/components/login.component.js
+++ frontend/src/components/login.component.js
@@ -9,7 +9,7 @@ export default class Login extends Component {
   render() {
     return (
       <div style={{ marginTop: 20 }}>
-        <GoogleAPI clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com">
+        <GoogleAPI clientId="363035258556-m8naekolmp41h1097cj7gdar2u2kdnn8.apps.googleusercontent.com">
           <GoogleLogin />
         </GoogleAPI>
       </div>
